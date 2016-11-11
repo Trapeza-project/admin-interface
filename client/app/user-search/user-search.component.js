8478d3bb34da3088779eb71021ca1cffdc793c9b
Joakim Larsson <joakimlarsson92@gmail.com>
2016-11-11 01:25:14 +0100

    Added simple layout for search

--- client/app/user-search/user-search.component.js
+++ client/app/user-search/user-search.component.js
@@ -4,6 +4,8 @@ import routing from './user-search.routes';
 
 export class UserSearchController {
 
+  searchText = "";
+  showResult = false;
   /*@ngInject*/
   constructor($http, socket) {
     this.$http = $http;
@@ -11,14 +13,22 @@ export class UserSearchController {
   }
 
   validate(evt) {
-    var theEvent = evt || window.event;
-    var key = theEvent.keyCode || theEvent.which;
-    key = String.fromCharCode( key );
-    var regex = /[0-9]|\./;
-    if( !regex.test(key) ) {
-      theEvent.returnValue = false;
-      if(theEvent.preventDefault) theEvent.preventDefault();
+      var theEvent = evt || window.event;
+      var key = theEvent.keyCode || theEvent.which;
+      key = String.fromCharCode(key);
+      var regex = /[0-9]|\./;
+      if (!regex.test(key) || this.searchText.length >= 12) {
+        theEvent.returnValue = false;
+        if (theEvent.preventDefault) theEvent.preventDefault();
+      }
     }
+    search() {
+      console.log('search', this.searchText);
+      if(this.searchText.length < 12) {
+        this.showResult = false;
+        return;
+      }
+      this.showResult = true;
   }
 }
 
@@ -26,6 +36,7 @@ export default angular.module('adminInterfaceApp.user-search', [uiRouter])
   .config(routing)
   .component('userSearch', {
     template: require('./user-search.html'),
-    controller: UserSearchController
+    controller: UserSearchController,
+    controllerAs: 'userSearchCtrl'
   })
   .name;
